18c7997ccbbf149b0701ddee1c8cb3a40f4ba83b
Mohammad <18.mamali@gmail.com>
2021-08-30 13:42:57 +0430

    change main colors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require('path')
 
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('postcss-hexrgba'),
+  require('autoprefixer')
+]
+
 module.exports = {
   entry: './app/assets/scripts/App.js',
   output: {
@@ -22,20 +31,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', {loader: 'postcss-loader',
-         options:
-         {
-          postcssOptions: {
-            plugins: [
-              'postcss-import',
-              'postcss-mixins',
-              'postcss-simple-vars',
-              'postcss-nested',
-              'postcss-hexrgba',
-              'autoprefixer'
-            ]
-          }
-         }}]
+        use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins }}}]
       }
     ]
   }
