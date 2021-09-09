9bd0914596332a8be7bfb0d239d1e8b21a24f491
Mohammad <18.mamali@gmail.com>
2021-09-09 19:23:40 +0430

    Completed revel on scroll for skill's image

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,13 @@
 import css from '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll';
 
+
+new RevealOnScroll(document.querySelectorAll(".skill-image"), 80)
+new RevealOnScroll(document.querySelectorAll(".python"), 60)
 new MobileMenu();
+
+
 if (module.hot) {
   module.hot.accept()
 }
